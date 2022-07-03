5045603cffc773879a7051d1ea391879c15c64b9
flumiie <ferickwar@gmail.com>
2022-07-03 23:31:50 +0700

    Hover links animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './styles/App.css';
 
+import NavbarImage from './images/navbar_links.png';
+
 function App() {
   return (
-    <div style={{ flex: 1, backgroundColor: 'red' }}>
-      <>Hello</>
+    <div className='main-container'>
+      <div>
+        {/* Header font goes here */}
+        <img src={NavbarImage} alt='' id='navbar-home' />
+        <img src={NavbarImage} alt='' id='navbar-works' />
+        <img src={NavbarImage} alt='' id='navbar-contacts' />
+      </div>
     </div>
   );
 }
